d0ce85743ea9014ee0677698baa5c17844ce5cf6
Cam Stuart <cam.asoftware@gmail.com>
2018-11-23 13:56:18 +1100

    remove console log

--- src/components/App.js
+++ src/components/App.js
@@ -6,58 +6,73 @@ import Footer from './Footer'
 import getApiURL from '../config/api'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.getHodlClubMembers = this.getHodlClubMembers.bind(this)
-    this.getHodlOGMembers = this.getHodlOGMembers.bind(this)
-    this.state = {
-      showMe: true,
-      hodlClub: [],
-      hodlOG: []
+    constructor(props) {
+      super(props)
+      this.getHodlClubMembers = this.getHodlClubMembers.bind(this)
+      this.getHodlOGMembers = this.getHodlOGMembers.bind(this)
+      this.state = {
+        showMe: true,
+        hodlClub: [],
+        hodlOG: []
+      }
+      this.apiBasePath = getApiURL()
+    }
+    componentDidMount() {
+      this.getHodlClubMembers()
+      this.getHodlOGMembers()
     }
 
-    this.apiBasePath = getApiURL()
+    /**
+     *  Fetches the hodl club from the API
+     */
+    async getHodlClubMembers(sort) {
+      if (this.state.loading) return
+      const url = `${this.apiBasePath}/hodlers`
+      fetch(url)
+        .then(response => response.json())
+        .catch(this.setState({
+          hodlClub: [],
+          loading: false
+        }))
+        .then(data => this.setState({
+          hodlClub: data,
+          loading: false
+        }))
+    }
 
-    console.log('+ base path:', this.apiBasePath)
+    async getHodlOGMembers(sort) {
+      if (this.state.loadingOG) return
+      const url = `${this.apiBasePath}/hodlers?tier=OG`
+      fetch(url)
+        .then(response => response.json())
+        .catch(this.setState({
+          hodlOG: [],
+          loading: false
+        }))
+        .then(data => this.setState({
+          hodlOG: data,
+          loadingOG: false
+        }))
+    }
 
-  }
-  componentDidMount() {
-    this.getHodlClubMembers()
-    this.getHodlOGMembers()
-  }
+    render() {
+      return ( < div className = "App" >
+        <
+        Nav / >
+        <
+        hr / >
+        <
+        Main hodlClub = {
+          this.state.hodlClub
+        }
+        hodlOG = {
+          this.state.hodlOG
+        }
+        /> <
+        Footer / >
+        <
+        /div>)
+      }
+    }
 
-  /**
-   *  Fetches the hodl club from the API
-   */
-  async getHodlClubMembers(sort) {
-    if (this.state.loading) return
-    const url = `${this.apiBasePath}/hodlers`
-    fetch(url)
-      .then(response => response.json())
-      .catch(this.setState({ hodlClub: [], loading: false }))
-      .then(data => this.setState({ hodlClub: data, loading: false }))
-  }
-
-  async getHodlOGMembers(sort) {
-    if (this.state.loadingOG) return
-    const url = `${this.apiBasePath}/hodlers?tier=OG`
-    fetch(url)
-      .then(response => response.json())
-      .catch(this.setState({ hodlOG: [], loading: false }))
-      .then(data => this.setState({ hodlOG: data, loadingOG: false }))
-  }
-
-  render() {
-    return (<div className="App">
-      <Nav />
-      <hr />
-      <Main
-        hodlClub={this.state.hodlClub}
-        hodlOG={this.state.hodlOG}
-      />
-      <Footer />
-    </div>)
-  }
-}
-
-export default App
+    export default App
